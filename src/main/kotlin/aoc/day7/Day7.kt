d93008dc7515df90d7999142eece2052f0433e4d
Z-Kris <kristo190@hotmail.com>
2021-12-07 11:16:27 +0200

    Change one reference to MidCrabs to the normal Crabs as it is invalid.

--- src/main/kotlin/aoc/day7/Day7.kt
+++ src/main/kotlin/aoc/day7/Day7.kt
@@ -14,7 +14,7 @@ object Day7 : Puzzle<Crabs, Int>(7) {
     override fun Sequence<String>.parse() = single().split(',').map(String::toInt).sorted()
 
     private inline fun Crabs.minOf(middle: MidCrabs, transformer: FuelTransformer) = middle.minOf { sum(it, transformer) }
-    private inline fun MidCrabs.sum(position: Int, transformer: FuelTransformer) = sumOf { transformer(abs(it - position)) }
+    private inline fun Crabs.sum(position: Int, transformer: FuelTransformer) = sumOf { transformer(abs(it - position)) }
 
     override fun Crabs.solvePartOne() = minOf(medianValues(), Int::self)
     override fun Crabs.solvePartTwo() = minOf(meanValues(), Int::incrementingSum)
