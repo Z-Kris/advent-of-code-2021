6d1fc8c47ce149d4287ffcfb5ad1b33862c39f31
Z-Kris <kristo190@hotmail.com>
2021-12-07 10:25:58 +0200

    Change day 7 solution to a mathematical one, rather than bruteforced.

--- src/main/kotlin/aoc/Extensions.kt
+++ src/main/kotlin/aoc/Extensions.kt
@@ -2,6 +2,9 @@
 
 package aoc
 
+import kotlin.math.ceil
+import kotlin.math.floor
+
 /**
  * @author Kris | 07/12/2021
  */
@@ -13,3 +16,13 @@ inline fun <T> T.chainRepeat(times: Int, action: (Int) -> Unit): T {
 inline fun <K> Map<K, Long>.getOrZero(key: K) = getOrDefault(key, 0)
 
 inline fun <T> Iterable<T>.forEachFiltered(filter: (T) -> Boolean, action: (T) -> Unit) = forEach { element -> if (filter(element)) action(element) }
+
+inline fun <T> List<T>.medianValues(): List<T> = if (size and 0x1 == 0x1) {
+    listOf(get((size + 1) / 2))
+} else {
+    listOf(get((size - 1) / 2), get(size / 2))
+}
+
+inline fun List<Int>.meanValues(): List<Int> = with(sum() / size.toDouble()) {
+    listOf(floor(this).toInt(), ceil(this).toInt())
+}
--- src/main/kotlin/aoc/day7/Day7.kt
+++ src/main/kotlin/aoc/day7/Day7.kt
@@ -1,27 +1,24 @@
 package aoc.day7
 
 import aoc.Puzzle
+import aoc.meanValues
+import aoc.medianValues
 import kotlin.math.abs
 
 /**
  * @author Kris | 07/12/2021
  */
+@Suppress("NOTHING_TO_INLINE")
 object Day7 : Puzzle<Crabs, Int>(7) {
-    override fun Sequence<String>.parse(): Crabs {
-        val positions = single().split(',').map(String::toInt)
-        val farthestPosition = positions.maxOf { it }
-        return Crabs(positions, farthestPosition)
-    }
+    override fun Sequence<String>.parse(): Crabs = Crabs(single().split(',').map(String::toInt).sorted())
 
-    private inline fun Crabs.computeLeastAmountOfSteps(transformer: FuelTransformer) =
-        positionRange.minOf { position -> positions.sumOf { transformer(abs(it - position)) } }
+    private inline fun Crabs.computeLeastAmountOfSteps(inputs: List<Int>, transformer: FuelTransformer): Int =
+        inputs.minOf { position -> positions.sumOf { transformer(abs(it - position)) } }
 
-    override fun Crabs.solvePartOne() = computeLeastAmountOfSteps(Int::toInt)
-    override fun Crabs.solvePartTwo() = computeLeastAmountOfSteps(Int::incrementingSum)
+    override fun Crabs.solvePartOne() = computeLeastAmountOfSteps(positions.medianValues(), Int::toInt)
+    override fun Crabs.solvePartTwo() = computeLeastAmountOfSteps(positions.meanValues(), Int::incrementingSum)
 }
 
-data class Crabs(val positions: List<Int>, private val farthestPosition: Int) {
-    val positionRange: IntRange get() = 0 until farthestPosition
-}
+data class Crabs(val positions: List<Int>)
 private val Int.incrementingSum get() = this * (this + 1) / 2
 private typealias FuelTransformer = (distance: Int) -> Int
