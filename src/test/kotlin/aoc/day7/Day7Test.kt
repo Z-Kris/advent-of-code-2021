d1d6c168391d86190c0a5872500d049a7d8557ad
Z-Kris <kristo190@hotmail.com>
2021-12-08 00:32:04 +0200

    Import kotlin.test for day 7 tests.

--- src/test/kotlin/aoc/day7/Day7Test.kt
+++ src/test/kotlin/aoc/day7/Day7Test.kt
@@ -3,6 +3,7 @@ package aoc.day7
 import aoc.day7.Day7.solvePartOne
 import aoc.day7.Day7.solvePartTwo
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 /**
  * @author Kris | 07/12/2021
@@ -12,13 +13,13 @@ internal class Day7Test {
 
     @Test
     fun partOne() {
-        kotlin.test.assertEquals(37, Day7.parse(testInput).solvePartOne())
-        kotlin.test.assertEquals(347_509, Day7.parse().solvePartOne())
+        assertEquals(37, Day7.parse(testInput).solvePartOne())
+        assertEquals(347_509, Day7.parse().solvePartOne())
     }
 
     @Test
     fun partTwo() {
-        kotlin.test.assertEquals(168, Day7.parse(testInput).solvePartTwo())
-        kotlin.test.assertEquals(98_257_206, Day7.parse().solvePartTwo())
+        assertEquals(168, Day7.parse(testInput).solvePartTwo())
+        assertEquals(98_257_206, Day7.parse().solvePartTwo())
     }
 }
