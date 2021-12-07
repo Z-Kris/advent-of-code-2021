56905d6ac21fb69ec51db2cc4edefcc7d2f812bb
Z-Kris <kristo190@hotmail.com>
2021-12-07 11:23:40 +0200

    Rename day 7 minOf to minSumOf to reflect clearer naming.

--- src/main/kotlin/aoc/day7/Day7.kt
+++ src/main/kotlin/aoc/day7/Day7.kt
@@ -13,11 +13,11 @@ import kotlin.math.abs
 object Day7 : Puzzle<Crabs, Int>(7) {
     override fun Sequence<String>.parse() = single().split(',').map(String::toInt).sorted()
 
-    private inline fun Crabs.minOf(middle: MidCrabs, transformer: FuelTransformer) = middle.minOf { sum(it, transformer) }
+    private inline fun Crabs.minSumOf(middle: MidCrabs, transformer: FuelTransformer) = middle.minOf { sum(it, transformer) }
     private inline fun Crabs.sum(position: Int, transformer: FuelTransformer) = sumOf { transformer(abs(it - position)) }
 
-    override fun Crabs.solvePartOne() = minOf(medianValues(), Int::self)
-    override fun Crabs.solvePartTwo() = minOf(meanValues(), Int::incrementingSum)
+    override fun Crabs.solvePartOne() = minSumOf(medianValues(), Int::self)
+    override fun Crabs.solvePartTwo() = minSumOf(meanValues(), Int::incrementingSum)
 }
 private val Int.incrementingSum get() = this * (this + 1) / 2
 private typealias FuelTransformer = (distance: Int) -> Int
