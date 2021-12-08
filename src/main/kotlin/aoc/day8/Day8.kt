eca4cea6d5063fe2f9a076a0610cca7d2c83e0ac
Z-Kris <kristo190@hotmail.com>
2021-12-08 09:58:42 +0200

    Clean up method naming in day 8.

--- src/main/kotlin/aoc/day8/Day8.kt
+++ src/main/kotlin/aoc/day8/Day8.kt
@@ -26,9 +26,9 @@ object Day8 : Puzzle<List<Code>, Int>(8) {
 data class Code(val segment: Segment, val digits: List<Digits>)
 data class Digits(val digits: Set<Char>) : Set<Char> by digits
 data class Segment(val numbers: List<Digits>) : List<Digits> by numbers {
-    private fun Digits.hasLetters(vararg letters: Char) = letters.all { it in this }
+    private fun Digits.hasAll(vararg letters: Char) = letters.all { it in this }
     private fun Digits.hasNone(vararg letters: Char) = letters.none { it in this }
-    private fun Digits.missing(vararg letters: Char) = first { it !in letters }
+    private fun Digits.findMissing(vararg letters: Char) = first { it !in letters }
     private fun Digits.isAny(vararg options: Digits) = options.any { it == this }
     private fun Digits.onlyOneIn(other: Digits) = count { it in other } == 1
     fun Digits.isPartOneDigit() = isAny(one, four, seven, eight)
@@ -36,24 +36,23 @@ data class Segment(val numbers: List<Digits>) : List<Digits> by numbers {
     private val a by lazy(NONE) { seven.first { it !in one } }
     private val b by lazy(NONE) { four.first { it !in three } }
     private val c by lazy(NONE) { one.first { it !in six } }
-    private val d by lazy(NONE) { four.missing(b, c, f) }
-    private val e by lazy(NONE) { two.missing(a, c, d, g) }
+    private val d by lazy(NONE) { four.findMissing(b, c, f) }
+    private val e by lazy(NONE) { two.findMissing(a, c, d, g) }
     private val f by lazy(NONE) { one.first { it != c } }
-    private val g by lazy(NONE) { three.missing(a, c, d, f) }
+    private val g by lazy(NONE) { three.findMissing(a, c, d, f) }
 
-    private val zero by lazySearch(size = 6) { !it.hasLetters(d) }
+    private val zero by lazySearch(size = 6) { !it.hasAll(d) }
     private val one by lazySearch(size = 2)
-    private val two by lazySearch(size = 5) { it.hasLetters(a, c, d, g) && it.hasNone(f) }
-    private val three by lazySearch(size = 5) { it.hasLetters(f, c) }
+    private val two by lazySearch(size = 5) { it.hasAll(a, c, d, g) && it.hasNone(f) }
+    private val three by lazySearch(size = 5) { it.hasAll(f, c) }
     private val four by lazySearch(size = 4)
     private val five by lazySearch(size = 5) { it.hasNone(c, e) }
     private val six by lazySearch(size = 6) { it.onlyOneIn(one) }
     private val seven by lazySearch(size = 3)
     private val eight by lazySearch(size = 7)
-    private val nine by lazySearch(size = 6) { !it.hasLetters(e) }
+    private val nine by lazySearch(size = 6) { !it.hasAll(e) }
 
     private val digits by lazy { listOf(zero, one, two, three, four, five, six, seven, eight, nine) }
-
     fun getDigit(digits: Digits): Int = requireNotNull(this.digits.indexOf(digits))
 }
 
