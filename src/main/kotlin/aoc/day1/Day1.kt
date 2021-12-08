eb5b3d0bc8e3dccbf5a7be935bc1a96306af5f40
Z-Kris <kristo190@hotmail.com>
2021-12-08 12:49:24 +0200

    Remove redundant comments.

--- src/main/kotlin/aoc/day1/Day1.kt
+++ src/main/kotlin/aoc/day1/Day1.kt
@@ -10,16 +10,6 @@ import aoc.Puzzle
 object Day1 : Puzzle<List<Int>, Int>(1) {
     override fun Sequence<String>.parse(): List<Int> = map(String::toInt).toList()
     override fun List<Int>.solvePartOne() = countIncrements(1)
-
-    /**
-     * Instead of taking the traditional approach of:
-     * ```kt
-     * windowed(size = 3).map(List<Int>::sum)
-     * ```
-     * We can reduce the number of computations necessary to solve the equation by checking **nth** element against **nth + offset**.
-     * This is possible because **(a, b, c)** <= **(b, c, d)** has two common values, them being **b** & **c**. Since the three would be summed,
-     * we can skip that part entirely and just check **a** against **d**.
-     */
     override fun List<Int>.solvePartTwo() = countIncrements(3)
     private inline fun <T : Comparable<T>> List<T>.countIncrements(offset: Int) = (0 until (size - offset)).count { get(it + offset) > get(it) }
 }
