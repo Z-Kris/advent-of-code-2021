619794d139a3b6e79a1f31568e6e86effc555b85
Z-Kris <kristo190@hotmail.com>
2021-12-09 10:27:08 +0200

    Rearrange some code to make use of more method references.

--- src/main/kotlin/aoc/day9/Day9.kt
+++ src/main/kotlin/aoc/day9/Day9.kt
@@ -8,7 +8,6 @@ import aoc.Puzzle
  */
 @OptIn(ExperimentalStdlibApi::class)
 object Day9 : Puzzle<LavaTubes, Int>(9) {
-    private val offsets = listOf(Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0))
     override fun Sequence<String>.parse(): LavaTubes {
         val tubes = toList().map { it.map(Character::getNumericValue) }
         val maxRow = tubes.size
@@ -18,19 +17,14 @@ object Day9 : Puzzle<LavaTubes, Int>(9) {
     }
 
     override fun LavaTubes.solvePartOne(): Int = findLowPoints().sumOf { this[it] + 1 }
-    override fun LavaTubes.solvePartTwo(): Int =
-        findLowPoints().map { computeConnectedPoints(it, emptyVisitedNodes()) }.sortedDescending().take(3).reduce(Int::times)
+    override fun LavaTubes.solvePartTwo(): Int = findLowPoints().map(::computeConnectedPoints).sortedDescending().take(3).reduce(Int::times)
 
     private fun LavaTubes.findLowPoints(): List<Point> = points.filter { this[it] < requireNotNull(findNeighbours(it).minOrNull()) }
     private fun LavaTubes.findNeighbours(point: Point) = findNeighbouringPoints(point).map(::get)
-    private fun LavaTubes.findNeighbouringPoints(point: Point) = offsets.map(point::merge).filter(::contains)
-
-    private fun LavaTubes.computeConnectedPoints(point: Point, visited: Array<BooleanArray>): Int {
-        if (visited[point.x][point.y] || this[point] == 9) return 0
-        visited[point.x][point.y] = true
-        return 1 + findNeighbouringPoints(point).sumOf { computeConnectedPoints(it, visited) }
-    }
 }
+
+private val offsets = listOf(Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0))
+private fun LavaTubes.findNeighbouringPoints(point: Point) = offsets.map(point::merge).filter(::contains)
 
 data class LavaTubes(val tubes: List<List<Int>>, val maxRow: Int, val maxCol: Int) {
     private val rowRange get() = 0 until maxRow
@@ -39,5 +33,11 @@ data class LavaTubes(val tubes: List<List<Int>>, val maxRow: Int, val maxCol: In
     operator fun get(x: Int, y: Int) = tubes[x][y]
     operator fun get(point: Point) = tubes[point.x][point.y]
     operator fun contains(point: Point) = point.x in rowRange && point.y in colRange
-    fun emptyVisitedNodes() = Array(maxRow) { BooleanArray(maxCol) }
+    private fun emptyVisitedNodes() = Array(maxRow) { BooleanArray(maxCol) }
+
+    fun computeConnectedPoints(point: Point, visited: Array<BooleanArray> = emptyVisitedNodes()): Int {
+        if (visited[point.x][point.y] || this[point] == 9) return 0
+        visited[point.x][point.y] = true
+        return 1 + findNeighbouringPoints(point).sumOf { computeConnectedPoints(it, visited) }
+    }
 }
