59648257fdfb602787db26837c5d107ea71aef58
Z-Kris <kristo190@hotmail.com>
2021-12-16 19:31:08 +0200

    Remove unnecessary buffer wrap in read only bit buffer class.

--- src/main/kotlin/aoc/ReadOnlyBitBuffer.kt
+++ src/main/kotlin/aoc/ReadOnlyBitBuffer.kt
@@ -1,15 +1,12 @@
 package aoc
 
-import java.nio.ByteBuffer
-
 /**
  * @author Kris | 16/12/2021
  */
-class ReadOnlyBitBuffer(private val bytes: ByteArray) {
-    private val buffer = ByteBuffer.wrap(bytes)
+class ReadOnlyBitBuffer(private val buffer: ByteArray) {
     private var bitReaderIndex = 0
 
-    fun remaining(): Int = (bytes.size shl 3) - bitReaderIndex
+    fun remaining(): Int = (buffer.size shl 3) - bitReaderIndex
 
     fun readBits(numBits: Int): Int {
         if (numBits < 0 || numBits > 32) throw IllegalStateException("Number of bits must remain between 0 and 32(inclusive).")
@@ -19,14 +16,14 @@ class ReadOnlyBitBuffer(private val bytes: ByteArray) {
         var value = 0
         bitReaderIndex += bitsLeftToRead
         while (bitsLeftToRead > bitOffset) {
-            value += (buffer.get(bytePos++).toInt() and bitMasks[bitOffset]) shl bitsLeftToRead - bitOffset
+            value += (buffer[bytePos++].toInt() and bitMasks[bitOffset]) shl bitsLeftToRead - bitOffset
             bitsLeftToRead -= bitOffset
             bitOffset = 8
         }
         value += if (bitOffset == bitsLeftToRead) {
-            buffer.get(bytePos).toInt() and bitMasks[bitOffset]
+            buffer[bytePos].toInt() and bitMasks[bitOffset]
         } else {
-            buffer.get(bytePos).toInt() shr bitOffset - bitsLeftToRead and bitMasks[bitsLeftToRead]
+            buffer[bytePos].toInt() shr bitOffset - bitsLeftToRead and bitMasks[bitsLeftToRead]
         }
         return value
     }
